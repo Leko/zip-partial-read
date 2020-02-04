a870bff5923a076e9e9bbea34ad36e34f24db89a
Leko <leko.noor@gmail.com>
2020-02-04 17:55:52 +0900

    tmp

--- cats.sh
+++ cats.sh
@@ -7,5 +7,5 @@ rm -rf $DIR
 mkdir -p $DIR
 
 for i in $(seq -w $COUNT); do
-    wget -O $DIR/cat${i}.png "http://placekitten.com/${i}00/${i}00"
+    wget -O $DIR/cat${i}.png "http://placekitten.com/${i}0/${i}0"
 done
